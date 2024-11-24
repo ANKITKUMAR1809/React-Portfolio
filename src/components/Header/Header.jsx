dbcac177a6d772f2bb4b7e2493e8b2ad1b713aa1
ANKITKUMAR1809 <skwedsak143@gmail.com>
2024-11-24 11:29:44 +0530

    new changes to my portfolio

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import HeaderImg from '../../assets/avatar1.png'
 import { RiYoutubeFill, RiLinkedinFill, RiGithubFill, RiMailFill } from '@remixicon/react'
 import './Header.css'
 const Header = () => {
@@ -9,12 +8,15 @@ const Header = () => {
                 <div className="info ">
                     <h3>THIS IS ME</h3>
                     <h1>ANKIT KUMAR</h1>
-                    <h2>I'm Front-end Developer. Currently, I am starving for Opportunity. Becoming MERN stack developer,
-                        Youtuber and Waiting for Job Opportunity or Internship.</h2>
+                    <h2>
+                        <p>
+                            MERN Stack Developer | Frontend Specialist | Aspiring Django Developer
+                        </p>
+                        <p>
+                            🎓 MCA Student | 🚀 Open to Exciting Job Opportunities
+                        </p>
+                        With a strong foundation in frontend development and expertise in the MERN stack, I craft seamless, user-friendly web applications. Currently, I'm expanding my skill set by diving into Python and the Django framework. I’m passionate about building impactful digital experiences and eager to contribute my skills to innovative projects.</h2>
                     <a href="#contact" id="hirebtn">HIRE ME</a>
-                </div>
-                <div className="img">
-                    <img src={HeaderImg} alt="" />
                 </div>
             </div>
             <a href="#header" id="top">Top</a>
